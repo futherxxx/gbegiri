24b013b92b1389ef4032afd51939e95d18ee36c7
futherxxx <futherxxx@gmail.com>
2017-08-27 15:41:19 +0100

    initial commit of version 0.90 gbegiri food staples

---
+++ Scripts/customscript2.js
@@ -0,0 +1,16 @@
+﻿//cart-js -->
+
+w3ls.render();
+
+w3ls.cart.on('w3sb_checkout', function (evt) {
+    var items, len, i;
+
+    if (this.subtotal() > 0) {
+        items = this.items();
+
+        for (i = 0, len = items.length; i < len; i++) {
+        }
+    }
+});
+
+ //cart-js -->
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GbegiriWeb.Startup))]
+namespace GbegiriWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
